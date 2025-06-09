307aa4de23bc2c52da03d7d31023dba73642bff1
vyshakhtp <vyshakhtp782@gmail.com>
2025-06-09 20:15:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+
+
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         hello world
+        </p>
+
+        <button onClick={() => setCount(count + 1)}> You clicked {count} times</button>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
